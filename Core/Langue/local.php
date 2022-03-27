9f34c4c1d18ce3035232985f021aeb98017f3d40
nitneuq <quentin@miladz.eu>
2022-03-28 00:27:27 +0200

    Ajout du fichier de langue

---
+++ Core/Langue/local.php
@@ -0,0 +1,12 @@
+<?php
+
+
+/** Menu */
+define('MENU_HOME', 'Accueil');
+define('MENU_IMPORT', 'Importation');
+define('MENU_SESSION', 'Connexion');
+
+
+/** IMPORTATION */
+define('F_SELECT_CATA', 'essais');
+define('F_PAGE_IMPORT', 'Pages à importé');
